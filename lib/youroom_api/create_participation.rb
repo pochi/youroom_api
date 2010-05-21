bbe30516ed1e26917da6ca7c90a44e0178f30b19
Yuichi Kuroda <yuuichi_kuroda@mufg.jp>
2010-05-21 21:41:24 +0900

    [xxx] Refactoring: delete file

--- lib/youroom_api/create_participation.rb
+++
@@ -1,8 +0,0 @@
-module Youroom
-  class CreateParticipation < Participation
-    def path
-      File.join(@path, 'redmine', 'participation', 'create')
-    end
-
-  end
-end
--- lib/youroom_api/destroy_participation.rb
+++
@@ -1,7 +0,0 @@
-module Youroom
-  class DestroyParticipation < Participation
-    def path
-      File.join(@path, 'redmine', 'participation', 'destroy')
-    end
-  end
-end
