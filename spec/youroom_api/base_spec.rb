45e3b98fa0a7432ddba128fe9d5e289a2c0a9642
pochi <pochi.black@gmail.com>
2010-07-25 17:40:57 +0900

    Delete spec file

--- spec/youroom_api/base_spec.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path("../spec_helper", File.dirname(__FILE__))
-
-describe Youroom::Base do
-  describe "enterprise_host" do
-    subject { Youroom::Base.new.send(:enterprise_host) }
-    it { should == "https://enterprise.youroom.in/"}
-  end
-end
